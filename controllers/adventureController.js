741c3695df652fc1b01a8fafb9154f98b6c68988
Jasmine Seitz <jrae.seitz@gmail.com>
2019-02-02 22:14:22 -0600

    get all and get by Id for adventures

--- controllers/adventureController.js
+++ controllers/adventureController.js
@@ -1,67 +1,91 @@
 const Adventure = require('../models/adventure')
 
 exports.create = function (req, res) {
-  const { name, description, image, nsfw } = req.body
-  const adventure = new Adventure({ name, description, image, nsfw, owner: req.email })
+    const { name, description, image, nsfw } = req.body
+    const adventure = new Adventure({ name, description, image, nsfw, owner: req.user.id })
 
-  adventure.save(function (err) {
-    if (err) {
-      res.status(500)
-        .json({
-          message: 'Error adding new adventure, please try again',
-          error: err
-        })
-    } else {
-      res.status(201).send()
-    }
-  })
+    adventure.save(function (err) {
+        if (err) {
+            res.status(500)
+                .json({
+                    message: 'Error adding new adventure, please try again',
+                    error: err
+                })
+        } else {
+            res.status(201).send()
+        }
+    })
 }
 
-exports.get = function (req, res) {
-  Adventure.find({ owner: req.email }, function (err, docs) {
-    if (err) {
-      res.status(500)
-        .json({
-          message: 'Error getting your adventures, please try again',
-          error: err
-        })
-    } else {
-      res.status(200)
-        .json(docs)
-    }
-  })
+exports.getAll = function (req, res) {
+    Adventure.find({ published: true }, function (err, docs) {
+        if (err) {
+            res.status(500)
+                .json({
+                    message: 'Error getting adventures, please try again',
+                    error: err
+                })
+        } else {
+            res.status(200)
+                .json(docs)
+        }
+    })
+}
+
+exports.getById = function (req, res) {
+    const { adventureId } = req.params
+    const options = { _id: adventureId }
+    Adventure.findOne(options, function (err, doc) {
+        if (err) {
+            res.status(500)
+                .json({
+                    message: 'Error getting adventure, please try again',
+                    error: err
+                })
+        }
+        else {
+            if (!doc.published && req.user.id !== doc.owner) {
+                res.status(401).send()
+            }
+            else {
+                res.status(200)
+                    .json(doc)
+            }
+
+        }
+    })
 }
 
 exports.update = function (req, res) {
-  const { adventureId } = req.params
+    const { adventureId } = req.params
 
-  Adventure.findByIdAndUpdate(adventureId, req.body, { new: true }, function (err, adventure) {
-    if (err) {
-      res.status(500)
-        .json({
-          message: 'Error updating adventure, please try again',
-          error: err
-        })
-    } else {
-      console.log(adventure)
-      res.status(200)
-        .json(adventure)
-    }
-  })
+    Adventure.findByIdAndUpdate(adventureId, req.body, { new: true }, function (err, adventure) {
+        if (err) {
+            res.status(500)
+                .json({
+                    message: 'Error updating adventure, please try again',
+                    error: err
+                })
+        } else {
+            console.log(adventure)
+            res.status(200)
+                .json(adventure)
+        }
+    })
 }
 
 exports.delete = function (req, res) {
-  const { adventureId } = req.params
+    const { adventureId } = req.params
 
-  Adventure.deleteOne({ _id: adventureId }, function (err) {
-    if (err) {
-      res.status(500)
-        .json({
-          message: 'Error deleteing adventure, please try again',
-          error: err
-        })
-    } else {
-      res.status(200).send()
-    }
-  })
+    Adventure.deleteOne({ _id: adventureId }, function (err) {
+        if (err) {
+            res.status(500)
+                .json({
+                    message: 'Error deleteing adventure, please try again',
+                    error: err
+                })
+        } else {
+            res.status(200).send()
+        }
+    })
 }
--- routes/adventures.js
+++ routes/adventures.js
@@ -5,6 +5,7 @@ const withAuth = require('../lib/middleware')
 router.post('/', withAuth, adventure.create)
 router.delete('/:adventureId', withAuth, adventure.delete)
 router.put('/:adventureId', withAuth, adventure.update)
-router.get('/', withAuth, adventure.get)
+router.get('/', withAuth, adventure.getAll)
+router.get('/:adventureId', withAuth, adventure.getById)
 
 module.exports = router
