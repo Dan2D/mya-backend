0599085061396611e9708963e407ed589abbda00
Jasmine Seitz <jrae.seitz@gmail.com>
2019-02-02 22:14:46 -0600

    get all adventures for user

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,8 @@
 const router = require('express').Router()
 const users = require('../controllers/usersController')
+const withAuth = require('../lib/middleware')
 
 router.post('/', users.create)
+router.get('/:userId/adventures', withAuth, users.getAdventures)
 
 module.exports = router
